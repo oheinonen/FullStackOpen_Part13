fa72130668f545cbce854f56f0d7a777a9b69558
Oskari Heinonen <oskari.l.heinonen@helsinki.fi>
2024-09-04 18:11:53 +0300

    search authors too

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -45,11 +45,19 @@ const authChecker = async (req, res, next) => {
 router.get('/', async (req, res) => {
   const where = {}
   if (req.query.search) {
-    where.title = {
-      [Op.iLike]: `%${req.query.search}%`
-    }
+    where[Op.or] = [
+      {
+        title: {
+          [Op.iLike]: `%${req.query.search}%`
+        }
+      },
+      {
+        author: {
+          [Op.iLike]: `%${req.query.search}%`
+        }
+      }
+    ]
   }
-
 
   const blogs = await Blog.findAll({
     attributes: { exclude: ['userId'] },
