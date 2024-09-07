606fab74bd5af3d240ddd13befe50088aa49eaa6
Oskari Heinonen <oskari.l.heinonen@helsinki.fi>
2024-09-07 12:01:06 +0300

    add sessions and disabled users

--- models/index.js
+++ models/index.js
@@ -1,14 +1,18 @@
 const Blog = require('./blog')
 const User = require('./user')
 const ReadingList = require('./readingList')
+const Session = require('./session')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 Blog.belongsToMany(User, { through: ReadingList, as: 'readers' })
 User.belongsToMany(Blog, { through: ReadingList, as: 'readings' })
+User.hasMany(Session)
+Session.belongsTo(User)
 
 module.exports = {
   Blog,
   User,
-  ReadingList
+  ReadingList,
+  Session
 }
--- models/user.js
+++ models/user.js
@@ -22,6 +22,10 @@ User.init({
     type: DataTypes.STRING,
     allowNull: false
   },
+  isDisabled: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  }
 }, {
   sequelize,
   underscored: true,
